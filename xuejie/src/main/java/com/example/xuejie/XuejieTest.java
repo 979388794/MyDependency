8fac39b98678f49fe8410131105f878286e84afa
Henry Xue(薛捷) <henry.xue@QUECTEL.COM>
2023-11-22 16:39:09 +0800

    Initial commit

---
+++ xuejie/src/main/java/com/example/xuejie/XuejieTest.java
@@ -0,0 +1,28 @@
+package com.example.xuejie;
+
+/**
+ * @author: henry.xue
+ * @date: 2023-11-22
+ */
+public class XuejieTest {
+    /**
+     * 计算自身的平方
+     */
+public int calcutate(int number){
+
+    return number*number;
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
